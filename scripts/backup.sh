d49b75575c42713570b94f2d9c3046c63012bdf4
Prakhar Agnihotri <rounak316@gmail.com>
2020-04-27 06:33:46 +0530

    deploy

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,2 +1,2 @@
 # Backup
-docker exec landing-page_db_1 /usr/bin/mysqldump -u root --password=example ghost > ./backup/backup.sql
+docker exec landing-db /usr/bin/mysqldump -u root --password=example ghost > ./backup/backup.sql
